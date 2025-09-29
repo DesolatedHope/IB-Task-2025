abb00aab3a7fb17b1e1bc9d9f6a635b83fdbf76d
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2025-09-29 23:26:28 +0530

    Add WAContract class and standard template variable

--- variables.py
+++ variables.py
@@ -8,4 +8,14 @@ class TNContract(Enum):
     TN_CONTRACT_2 = "TN_Contract2_Redacted"
     TN_CONTRACT_3 = "TN_Contract3_Redacted"
     TN_CONTRACT_4 = "TN_Contract4_Redacted"
+
     TN_CONTRACT_5 = "TN_Contract5_Redacted"
+
+WA_STANDARD_TEMPLATE = "WA_Standard_Template_Redacted"
+class WAContract(Enum):
+    WA_CONTRACT_1 = "WA_Contract1_Redacted"
+    WA_CONTRACT_2 = "WA_Contract2_Redacted"
+    WA_CONTRACT_3 = "WA_Contract3_Redacted"
+    WA_CONTRACT_4 = "WA_Contract4_Redacted"
+
+    WA_CONTRACT_5 = "WA_Contract5_Redacted"
